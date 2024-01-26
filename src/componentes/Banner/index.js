0d0e430254172fec76b2ecd795373e7fabb2c487
leonardopramos <leonardopreczevskiramos@gmail.com>
2024-01-26 11:15:37 -0300

    Finalizando uma aula do curso

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="imagem-do-banner"></img>
+    </header>
+  );
+}
+export default Banner;
---
+++ src/componentes/Form/index.js
@@ -0,0 +1,62 @@
+import './styles.css';
+import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
+import { useState } from 'react';
+
+const Formulario = () => {
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    ' Inovação e Gestão',
+  ];
+  const [nome, setNome] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [time, setTime] = useState('');
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log('Foi submitido => ', nome, cargo, imagem, time);
+  };
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador:</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Imagem"
+          placeholder="Informe o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+          label="Time"
+          itens={times}
+        />
+        <Botao>Criar Card</Botao>
+      </form>
+    </section>
+  );
+};
+export default Formulario;
